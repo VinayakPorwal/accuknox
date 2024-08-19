5ed29c3fb84fcf9cddfdaf2d4afa70314ec46aae
vinayakporwal <vinayakporwal5522@gmail.com>
2024-08-20 01:46:46 +0530

    minor tweak

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,8 @@
 import { useSelector } from "react-redux";
-import Header from "./Components/header";
+import Header from "./Components/Header";
 import { Fragment, useState } from "react";
 import CategoryCard from "./Components/CategoryCard";
 import SideDrawer from "./Components/AddWidgetDrawer";
-
-// const App = () => {
-//   const { loading, error, data } = useSelector((state) => state.category);
-//   const [isDrawerOpen, setDrawerOpen] = useState(false);
-//   const [categories, setCategories] = useState([
-//     { name: "Cloud Accounts", text: "Connected (2)" },
-//     { name: "Cloud Account Risk Assessment", text: "Failed (1589)" },
-//   ]);
-
-//   const addWidget = (newWidgets) => {
-//     setCategories([...categories, ...newWidgets]);
-//   };
-
-//   const removeWidget = (widgetToRemove) => {
-//     setCategories(categories.filter((widget) => widget !== widgetToRemove));
-//   };
-
-//   if (loading) return <div>Loading...</div>;
-//   if (error) return <div>Error: {error}</div>;
-
-//   return (
-//     <div className="bg-blue-50">
-//       <div className="p-4 max-w-[1200px] mx-auto">
-//         <Header openDrawer={() => setDrawerOpen(true)} />
-//         <SideDrawer
-//           isOpen={isDrawerOpen}
-//           closeDrawer={() => setDrawerOpen(false)}
-//           addWidget={addWidget}
-//         />
-//         {data?.map((category) => (
-//           <Fragment key={category.id}>
-//             <CategoryCard
-//               id={category.id}
-//               name={category.name}
-//               widgets={category.widgets}
-//             />
-//           </Fragment>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
 
 const App = () => {
   const categories = useSelector((state) => state.category);
